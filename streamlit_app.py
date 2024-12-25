1e34c966d28b7a2b0c9abc5393bbdbb7bfe3ca21
burner-hashbrown <elapinel@my.chemeketa.edu>
2024-12-25 07:37:53 -0800

    Update streamlit_app.py
    moving code inside the loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -26,6 +27,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.text(smoothiefroot_response)
+        #st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True)
    
  #   st.write(ingredients_string)
     time_to_insert = st.button('Submit Order')
@@ -40,8 +45,4 @@ if ingredients_list:
     
     #st.write(my_insert_stmt)
     #st.stop()
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True)
+
